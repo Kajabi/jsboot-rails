f62b37f40739b616ff44406ac4b03467ab708c62
Brendon Murphy <xternal1+github@gmail.com>
2013-02-22 15:31:04 -0800

    Make key for jsboot_data_tag optional
    Pulls controller#action if not provided

--- lib/jsboot/rails/view_helpers.rb
+++ lib/jsboot/rails/view_helpers.rb
@@ -2,12 +2,20 @@ module Jsboot
   module Rails
 
     module ViewHelpers
-      def jsboot_data_tag(data, key)
+      def jsboot_data_tag(data, key = nil)
+        key ||= jsboot_default_key
         id = "jsboot-#{key}"
         id.tr!('#', '-')
+
         content_tag :script, :id => id, :class => "jsboot-data", :type => "application/json" do
           jsboot_json_escape data.to_json.html_safe
         end
+      end
+
+      private
+
+      def jsboot_default_key
+        [params[:controller], params[:action]].join("#")
       end
 
       def jsboot_json_escape(s)
